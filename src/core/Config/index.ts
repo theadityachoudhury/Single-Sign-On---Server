98da036457c55595a24497a0b3db6f44179923d4
Aditya Choudhury <adityasubham03@gmail.com>
2025-10-15 18:49:33 +0530

    Fixed server starting issue and circular dependency problem

--- src/core/Config/index.ts
+++ src/core/Config/index.ts
@@ -1,2 +1,2 @@
-export { default as corsOptions } from '@/Config/corsOptions.js';
 export { default as config } from '@/Config/config.js';
+export { default as corsOptions } from '@/Config/corsOptions.js';
--- src/core/Logger/index.ts
+++ src/core/Logger/index.ts
@@ -5,22 +5,37 @@ export { requestLogger } from './requestLogger.js';
 export const logger = {
     info: (msg: string, meta?: any) => {
         consola.info(msg);
+        if (meta) {
+            consola.info(meta);
+        }
         Logger.info(msg, meta);
     },
     success: (msg: string, meta?: any) => {
         consola.success(msg);
+        if (meta) {
+            consola.info(meta);
+        }
         Logger.info(msg, meta);
     },
     warn: (msg: string, meta?: any) => {
         consola.warn(msg);
+        if (meta) {
+            consola.info(meta);
+        }
         Logger.warn(msg, meta);
     },
     error: (msg: string, meta?: any) => {
         consola.error(msg);
+        if (meta) {
+            consola.info(meta);
+        }
         Logger.error(msg, meta);
     },
     debug: (msg: string, meta?: any) => {
-        consola.debug(msg);
-        Logger.debug(msg, meta);
+        consola.info(msg);
+        if (meta) {
+            consola.info(meta);
+        }
+        Logger.info(msg, meta);
     },
 };
